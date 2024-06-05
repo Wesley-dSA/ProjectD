731a4766ead9faf4a368253c39f5d22853c4794b
WESLEY  DOS SANTOS ARAGAO <0220482312009@fatectq.local>
2024-06-05 16:28:13 -0300

    ajustes

---
+++ TRABALHO PW/JavaScript/scriptInicial.js
@@ -0,0 +1,42 @@
+var items = [];
+
+        function addItem() {
+            var itemName = document.getElementById('itemName');
+            var itemQuantity = document.getElementById('itemQuantity');
+            var itemExpiry = document.getElementById('itemExpiry');
+            var itemList = document.getElementById('itemList');
+
+            var itemDiv = document.createElement('div');
+            itemDiv.textContent = `Produto: ${itemName.value}, Quantidade: ${itemQuantity.value}, Validade: ${itemExpiry.value}`;
+            itemDiv.className = 'item';
+
+            var removeButton = document.createElement('button');
+            removeButton.textContent = 'Remover';
+            removeButton.onclick = function() {
+                var index = items.indexOf(itemName.value);
+                if (index > -1) {
+                    items.splice(index, 1);
+                }
+                itemList.removeChild(itemDiv);
+            };
+
+            itemDiv.appendChild(removeButton);
+            itemList.appendChild(itemDiv);
+
+            items.push({name: itemName.value, div: itemDiv});
+            itemName.value = '';
+            itemQuantity.value = '';
+            itemExpiry.value = '';
+        }
+
+        function searchItem() {
+            var searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].name.toLowerCase().includes(searchInput)) {
+                    items[i].div.style.display = '';
+                } else {
+                    items[i].div.style.display = 'none';
+                }
+            }
+        }
