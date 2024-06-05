ca18185e715f40aeb57a9f6d853075d6284fe4f1
WESLEY  DOS SANTOS ARAGAO <0220482312009@fatectq.local>
2024-06-05 17:01:13 -0300

    Ajustadissimo

--- TRABALHO PW/JavaScript/scriptInicial.js
+++ TRABALHO PW/JavaScript/scriptInicial.js
@@ -1,42 +1,49 @@
 var items = [];
 
-        function addItem() {
-            var itemName = document.getElementById('itemName');
-            var itemQuantity = document.getElementById('itemQuantity');
-            var itemExpiry = document.getElementById('itemExpiry');
-            var itemList = document.getElementById('itemList');
+function formatDate(date) {
+    var parts = date.split('-');
+    return `${parts[2]}/${parts[1]}/${parts[0]}`;
+}
 
-            var itemDiv = document.createElement('div');
-            itemDiv.textContent = `Produto: ${itemName.value}, Quantidade: ${itemQuantity.value}, Validade: ${itemExpiry.value}`;
-            itemDiv.className = 'item';
+function addItem() {
+    var itemName = document.getElementById('itemName');
+    var itemQuantity = document.getElementById('itemQuantity');
+    var itemExpiry = document.getElementById('itemExpiry');
+    var itemList = document.getElementById('itemList');
+    
+    var formattedExpiry = formatDate(itemExpiry.value);
 
-            var removeButton = document.createElement('button');
-            removeButton.textContent = 'Remover';
-            removeButton.onclick = function() {
-                var index = items.indexOf(itemName.value);
-                if (index > -1) {
-                    items.splice(index, 1);
-                }
-                itemList.removeChild(itemDiv);
-            };
+    var itemDiv = document.createElement('div');
+    itemDiv.textContent = `Produto: ${itemName.value}, Quantidade: ${itemQuantity.value}, Validade: ${formattedExpiry}`;
+    itemDiv.className = 'item';
 
-            itemDiv.appendChild(removeButton);
-            itemList.appendChild(itemDiv);
-
-            items.push({name: itemName.value, div: itemDiv});
-            itemName.value = '';
-            itemQuantity.value = '';
-            itemExpiry.value = '';
+    var removeButton = document.createElement('button');
+    removeButton.textContent = 'Remover';
+    removeButton.onclick = function() {
+        var index = items.indexOf(itemName.value);
+        if (index > -1) {
+            items.splice(index, 1);
         }
+        itemList.removeChild(itemDiv);
+    };
 
-        function searchItem() {
-            var searchInput = document.getElementById('searchInput').value.toLowerCase();
+    itemDiv.appendChild(removeButton);
+    itemList.appendChild(itemDiv);
 
-            for (var i = 0; i < items.length; i++) {
-                if (items[i].name.toLowerCase().includes(searchInput)) {
-                    items[i].div.style.display = '';
-                } else {
-                    items[i].div.style.display = 'none';
-                }
-            }
+    items.push({name: itemName.value, div: itemDiv});
+    itemName.value = '';
+    itemQuantity.value = '';
+    itemExpiry.value = '';
+}
+
+function searchItem() {
+    var searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].name.toLowerCase().includes(searchInput)) {
+            items[i].div.style.display = '';
+        } else {
+            items[i].div.style.display = 'none';
         }
+    }
+}
